a701d11756b22f59b2367f12ca53a36ebbd853bf
joshuatblack <joshuatblack@ufl.edu>
2014-02-13 03:13:38 -0500

    creating functionality to find recent wagers and courses for users in the dashboard view

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,8 +10,25 @@ class HomeController extends BaseController {
 	public function showDashboard()
 	{
 		$user = Auth::user();
+		$date = new Datetime;
 
-		return View::make('dashboard')->withUser($user);
+		// get the current session
+		$session = DB::table('sessions')
+			->where('startDate', '<=', $date)
+			->where('endDate', '>=', $date)
+			->pluck('id');
+
+		// Find the user's wager from the current session
+		$wager = $user->wagers()->where('session_id', '=', $session)->get();
+
+		// Find user's courses for the semester.
+		$courses = $user->courses;
+
+		return View::make('dashboard', [
+				'user' => $user,
+				'wager' => $wager,
+				'courses' => $courses
+			]);
 	}
 
 }
