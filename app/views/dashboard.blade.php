fd080631b9d1ff156ad49ac495bf95ee2dda67d0
joshuatblack <joshuatblack@ufl.edu>
2014-02-21 19:31:09 -0500

    adding basic geolocation functionality

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -4,6 +4,9 @@
 	<meta charset="UTF-8">
 	<title>Dashboard View</title>
 </head>
+<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+
 <body>
 	<!-- Links/Nav -->
 	<nav>
@@ -17,6 +20,49 @@
 	<!-- Header -->
 	<h1>Hi, {{ ucfirst($user->username) }}!</h1>
 
+	<a id="checkin" href="#">Checkin</a>
+	<p>Position Coordinates:</p>
+	Latitude: <span id="latitude"></span><br>
+	Longitude: <span id="longitude"></span>
 
+<script type="text/javascript">
+
+/* Testing script for user checkin */
+
+	// Toggle showLocation request
+	$(document).ready(function() {
+		$('#checkin').on('click', function() {
+			showLocation();
+		});
+	});
+
+	function showLocation() {
+		// one-shot position request
+		navigator.geolocation.getCurrentPosition(callback, errorHandler);
+	}
+
+	function callback(position) {
+		document.getElementById('latitude').innerHTML = position.coords.latitude;
+		document.getElementById('longitude').innerHTML = position.coords.longitude;
+	}
+
+	function errorHandler(error) {
+		switch(error.code) {
+			case error.PERMISSION_DENIED:
+				document.getElementById('latitude').innerHTML = 'Location service privledges denied. Please enable it to checkin.';
+				break;
+			case error.POSITION_UNAVAILABLE:
+				document.getElementById('latitude').innerHTML = 'Position unavailable, please try checking in in a few minutes.';
+				break;
+			case error.TIMEOUT:
+				document.getElementById('latitude').innerHTML = 'Request timed out, please check your network settings and try again.';
+				break;
+			case error.UNKNOWN_ERROR:
+				document.getElementById('latitude').innerHTML = 'Unknown error, please try again.';
+				break;
+		}
+	}
+
+</script>
 </body>
 </html>
