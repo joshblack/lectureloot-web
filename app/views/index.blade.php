fcf58986464de92d165b19a54e75cb77e670c747
joshuatblack <joshuatblack@ufl.edu>
2014-03-18 13:12:35 -0400

    changing font declarations on main page to adjust for new @font-face r ules

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -9,12 +9,6 @@
 	<h1 id="logo">Lecture Loot</h1>
 	<a id="login" href="login">Login</a>
 	<a id="register" href="register">Register</a>
-  <div>
-    <p class="normal">This is a font</p>
-    <p class="bold">This is a font</p>
-    <p class="normal-italic">This is a font</p>
-    <p class="bold-italic">This is a font</p>
-  </div>
   <div id="poster"></div>
   <video autoplay muted loop poster="/dist/img/phone.jpg" id="bgvid">
     <source src="/dist/videos/phone.webm" type="video/webm">
