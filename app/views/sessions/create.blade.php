edd7b90afdb84220c1ee7db995ec6c04f66db7f8
joshuatblack <joshuatblack@ufl.edu>
2014-03-20 11:49:23 -0400

    adding scalability property onto all main meta tags

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 	<link rel="stylesheet" href="/dist/css/production.min.css">
 </head>
 <body class="alternate-page cbp-spmenu-push">
